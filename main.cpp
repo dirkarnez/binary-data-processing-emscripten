13748327cf1cfa1d8a4c0ba4ea1d4b5539528127
Dirk Arnez <smalldirkalex@gmail.com>
2025-08-30 02:43:46 +0800

    - update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,47 @@
-#include <fnmatch.h>
-#include <string>
 #include <iostream>
-
+#include <string>
+#include <vector>
 #include <emscripten/bind.h>
+#include <emscripten/val.h>
+
 using namespace emscripten;
 
-int fnmatch_wrapped(std::string pattern, std::string input, int flags) {
-    return fnmatch(pattern.c_str(), input.c_str(), flags);
+val get_bytes(std::string input) {
+    std::vector<unsigned char> vals(input.length());
+    for (int i = 0; i < input.length(); i++)  //We interate through string passed and add to vectors
+    {
+        vals.push_back((unsigned char)input.at(i));
+    }
+    return val(typed_memory_view(vals.size(), vals.data()));
 }
 
+
+// This is the extra code you need to write to expose your function to JS
 EMSCRIPTEN_BINDINGS(my_module) {
-    function("fnmatch", &fnmatch_wrapped);
-    constant("FNM_PATHNAME", FNM_PATHNAME);
-    constant("FNM_NOESCAPE", FNM_NOESCAPE);
-    constant("FNM_PERIOD", FNM_PERIOD);
-    
-    constant("FNM_NOMATCH", FNM_NOMATCH);
-    
-    // constant("FNM_FILE_NAME", FNM_FILE_NAME);
-    // constant("FNM_LEADING_DIR", FNM_LEADING_DIR);
-    // constant("FNM_CASEFOLD", FNM_CASEFOLD);
-    // constant("FNM_EXTMATCH", FNM_EXTMATCH);
+    function("getBytes", &get_bytes);
 }
+
+// #include <fnmatch.h>
+// #include <string>
+// #include <iostream>
+
+// #include <emscripten/bind.h>
+// using namespace emscripten;
+
+// int fnmatch_wrapped(std::string pattern, std::string input, int flags) {
+//     return fnmatch(pattern.c_str(), input.c_str(), flags);
+// }
+
+// EMSCRIPTEN_BINDINGS(my_module) {
+//     function("fnmatch", &fnmatch_wrapped);
+//     constant("FNM_PATHNAME", FNM_PATHNAME);
+//     constant("FNM_NOESCAPE", FNM_NOESCAPE);
+//     constant("FNM_PERIOD", FNM_PERIOD);
+    
+//     constant("FNM_NOMATCH", FNM_NOMATCH);
+    
+//     // constant("FNM_FILE_NAME", FNM_FILE_NAME);
+//     // constant("FNM_LEADING_DIR", FNM_LEADING_DIR);
+//     // constant("FNM_CASEFOLD", FNM_CASEFOLD);
+//     // constant("FNM_EXTMATCH", FNM_EXTMATCH);
+// }
